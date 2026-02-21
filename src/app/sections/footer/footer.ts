483c9235c53b61f54c5bbed9db638d9d3ea5e431
Jacob <Arthursjacob01@gmail.com>
2026-02-21 16:01:59 -0500

    Add blog to footer

--- src/app/sections/footer/footer.ts
+++ src/app/sections/footer/footer.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {  MatIconButton, MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
-import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { faRss, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faFileLines } from '@fortawesome/free-solid-svg-icons';
 
@@ -23,5 +23,6 @@ export class FooterComponent {
   protected currentYear: number = new Date().getFullYear();
   protected faGithub: IconDefinition = faGithub;
   protected faLinkedin: IconDefinition = faLinkedin;
+  protected faRss: IconDefinition = faRss;
   protected faFileLines: IconDefinition = faFileLines;
 }
